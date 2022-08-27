013bd3ef4ffb2ccaf829a2ea4a48b7a7db803b21
Suanig <suanig88@gmail.com>
2022-08-27 18:27:29 +0300

    Create HWL2T2

---
+++ HWL2T2/Program.cs
@@ -0,0 +1,21 @@
+﻿int GetTD(int number)
+{
+    if (Math.Abs(number) > 99)
+    {
+      int result = 0;
+      while (Math.Abs(number) > 999)
+      number /= 10;
+      result = number % 10; 
+      return result;
+    }
+    else
+    {
+        Console.WriteLine("Третьего числа нет");
+        return 0;
+    }
+}
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Третья цифра числа {number} -> {Math.Abs(GetTD(number))}"); 

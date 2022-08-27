c3784e2cb786b6ef4ce67e0262695ff4b8e4416e
Suanig <suanig88@gmail.com>
2022-08-27 18:01:54 +0300

    Create HWL2T1

---
+++ HWL2T1/Program.cs
@@ -0,0 +1,37 @@
+﻿int GetSD(int number)
+{
+    return number / 10 % 10;
+}
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 1000)
+{
+    if (number > 99)
+    {
+       Console.WriteLine($"Вторая цифра числа {number} -> {GetSD(number)}"); 
+    }
+    else
+    {
+        if (number > -1000)
+        {
+            if (number < -99)
+            {
+               Console.WriteLine($"Вторая цифра числа {number} -> {GetSD(number) * -1}");  
+            }
+            else 
+            {
+                Console.WriteLine("Число не трехзначное");
+            }
+        }
+    else 
+    {
+       Console.WriteLine("Число не трехзначное"); 
+    }
+    }
+     
+}
+else 
+{
+    Console.WriteLine("Число не трехзначное");
+}

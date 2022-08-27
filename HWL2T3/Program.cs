7bdc157f4952a76ad7d5005055c65362df2088d5
Suanig <suanig88@gmail.com>
2022-08-27 19:30:06 +0300

    Create HWL2T3

---
+++ HWL2T3/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] WeekDay = {"Monday", "Tuersday", "Wednesday", "Thirsday", "Friday", "Saturday", "Sunday" };
+Console.WriteLine("Введите номер дня недели от 1 до 7");
+int number = int.Parse(Console.ReadLine());
+
+if (number > 5)
+    {
+        Console.WriteLine($"{WeekDay[number - 1]} - Выходной");
+    }
+else 
+{
+    Console.WriteLine($"{WeekDay[number - 1]} - Не выходной");
+}
